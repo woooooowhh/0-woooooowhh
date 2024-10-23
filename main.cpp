50494de40a48c9bcabe412d52b4331aac4912c06
LiangJingwen <abc_abc61@qq.com>
2024-10-23 09:15:57 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -4,4 +4,29 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+
+    int d;
+
+    if(a>b){
+        d = a;
+        a = b;
+        b = d;
+    }
+
+    if(a>c){
+        d = a;
+        a = c;
+        c = d;
+    }
+
+    if(b>c){
+        d = b;
+        b = c;
+        c = d;
+    }
+
+    result.min = a;
+    result.mid = b;
+    result.max = c;
+
 }
